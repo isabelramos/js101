81c2b5ec064c3ebb4b68245bbbf1816ec64dc2f7
Isabel Ramos <iramos1226@gmail.com>
2017-01-24 20:49:18 -0600

    arrays

--- day2.js
+++ day2.js
@@ -20,3 +20,84 @@ if(day === "Monday"){
 } else {
 	console.log("it's not monday.");
 }
+
+// ARRAY
+
+var fruit = ["cherries", "apples", "bananas"];
+var fruitString = fruit.join(" cats ");
+console.log("fruit string", fruitString);
+
+
+// STRING METHOD OF ARRAY
+
+var catColors = 'green,red,yellow,black';
+var catColorsArray = catColors.split(",");
+console.log("catColorsArray", catColorsArray);
+
+
+var reverseFruit = fruit.reverse();
+console.log("reverseFruit", reverseFruit);
+
+var alphaFruit = fruit.sort();
+console.log("fruits in alphabetical order", alphaFruit);
+
+var nums = [9,6,8,2,4,11,66];
+var numSort = nums.sort(
+	function(a,b){
+		return a-b;
+	}
+);
+console.log("sorting numbers in order", numSort);
+
+var animals = ["cats", "turtles", "fish", "bears", "tigers"];
+console.log("fish index", animals.indexOf("fish"));
+
+var animals2 = animals.slice(2,4);
+console.log("animals2", animals2);
+
+animals.splice(1,1);
+console.log("no turtles", animals);
+
+
+var colors = ["red", "blue", "gold", "pink", "orange"];
+
+// SORT, REVERSE, AND MAKE A STRING
+
+var finalAnswer = colors.sort().reverse().join(" cat ");
+console.log("finalAnswer", finalAnswer);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
